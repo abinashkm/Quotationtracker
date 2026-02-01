e52479c8194e53fa01d132afce3658a97fcc2141
Abinash Mohanty <abinashkummar333@gmail.com>
2026-02-01 19:47:12 +0530

    Initial commit

---
+++ src/main/java/com/abinash/quotationtracker/QuotationtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.abinash.quotationtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotationtrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuotationtrackerApplication.class, args);
+    }
+
+}
